f44138337ea271cc7e3852b832288dbc38beb8e2
tadrith <alexandermeyersaam@gmail.com>
2019-03-02 15:05:39 -0800

    Starting the new Lab 9

--- fmt/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tadrith/lcc/cis162cpp/fmt/fmt
-# Build directory: /home/tadrith/lcc/cis162cpp/fmt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("doc")
-subdirs("test")
